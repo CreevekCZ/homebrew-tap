a24e51c5eea852dbcc2f8bee20d403023d05847b
Jan Kožnárek <xryx1111@gmail.com>
2026-03-16 00:43:07 +0100

    Update afm-cli formula to version 0.2.0 with new URL and SHA256 checksum

--- afm-cli.rb
+++ afm-cli.rb
@@ -1,10 +1,10 @@
 class AfmCli < Formula
   desc "A Swift command-line interface tool for interacting with macOS Apple Intelligence foundation models"
   homepage "https://github.com/CreevekCZ/afm-cli"
-  url "https://github.com/CreevekCZ/afm-cli/releases/download/v0.1.1/afm-cli-v0.1.1-darwin-arm64.tar.gz"
-  sha256 "b7ee47cc0c69c1e6719d360233a1b20c865f73346f703b494f30fd6ca8437d87"
+  url "https://github.com/CreevekCZ/afm-cli/releases/download/v0.2.0/afm-cli-v0.2.0-darwin-arm64.tar.gz"
+  sha256 "6db53b3bd98d0b575710bb59a68deecce39a8312f9337493cc7b2992824f8ebc"
   license "MIT"
-  version "0.1.1"
+  version "0.2.0"
 
   depends_on :macos => :tahoe
 

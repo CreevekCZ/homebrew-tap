187384a686c138f2b2b79a9f10be849a0338cdb2
Jan Kožnárek <xryx1111@gmail.com>
2026-03-27 23:48:28 +0100

    vash 1.0.0: add Linux x86_64 support

--- vash.rb
+++ vash.rb
@@ -1,10 +1,18 @@
 class Vash < Formula
   desc "CLI for managing vas-hosting.cz hosting accounts"
   homepage "https://github.com/CreevekCZ/vas-hosting-cli"
-  url "https://github.com/CreevekCZ/vas-hosting-cli/releases/download/v1.0.0/vash-v1.0.0-darwin-arm64.tar.gz"
-  sha256 "e935c7641ff1e0c6209d126a8e7fe0c51fb7dddcb34f45cf6fe8920ea593724b"
-  license "MIT"
   version "1.0.0"
+  license "MIT"
+
+  on_macos do
+    url "https://github.com/CreevekCZ/vas-hosting-cli/releases/download/v1.0.0/vash-v1.0.0-darwin-arm64.tar.gz"
+    sha256 "e935c7641ff1e0c6209d126a8e7fe0c51fb7dddcb34f45cf6fe8920ea593724b"
+  end
+
+  on_linux do
+    url "https://github.com/CreevekCZ/vas-hosting-cli/releases/download/v1.0.0/vash-v1.0.0-linux-x86_64.tar.gz"
+    sha256 "6e31fe2b87567a83cb16c8213e242169b447d86855510875b2a65304edf990fa"
+  end
 
   def install
     bin.install "vash"
